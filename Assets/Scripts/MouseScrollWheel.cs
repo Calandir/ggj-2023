34e92b1344fe981746c30456e8399dd716d701d8
SpencerMckay <spencer.mckay@protonmail.com>
2023-02-04 17:19:11 +1300

    Fixed logic error in scrollwheel

--- Assets/Scripts/MouseScrollWheel.cs
+++ Assets/Scripts/MouseScrollWheel.cs
@@ -1,69 +1,35 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class MouseScrollWheel : MonoBehaviour
+public class ScreenMovement : MonoBehaviour
 {
-    private bool isScrolling = false;
-    private float scrollSpeed = 1.0f;
-    private float returnSpeed = 0.4f;
-    private float returnTimer = 1.0f;
-    private float returnDelay = 1.0f;
-    private float startPositionY;
-    private IEnumerator returnRoutine;
+    public float speed = 5f;
+    public float returnSpeed = 2f;
+    public float resetTime = 1f;
 
+    private Vector3 originalPosition;
+    private float timer;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        startPositionY = transform.position.y; //The initial position, may need to update this as the player extends roots/screen. 
+        originalPosition = transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        float scrollWheel = Input.GetAxis("Mouse ScrollWheel");
-
-        if (scrollWheel != 0) { //if the scrollwheel is active
-            isScrolling = true;
-            transform.position += new Vector3(0, scrollWheel * 0.5f * scrollSpeed, 0);
-            returnTimer = 0.0f;
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            timer = 0f;
+            transform.position += new Vector3(0f, speed * Time.deltaTime, 0f);
         }
-
-        else if (Input.GetKey(KeyCode.UpArrow)) { //if the up arrow is active
-            isScrolling = true;
-            transform.position += new Vector3(0, scrollSpeed * 0.005f, 0);
-            returnTimer = 0.0f;
-        }
-
-        else if (isScrolling == false && transform.position.y != startPositionY) { //if camera return conditions are met
-            if (returnRoutine == null) {
-                returnRoutine = ReturnToStartPosition();
-                StartCoroutine(returnRoutine);
+        else
+        {
+            timer += Time.deltaTime;
+            if (timer >= resetTime && Mathf.Round(transform.position.y) != Mathf.Round(originalPosition.y))
+            {
+                transform.position = Vector3.Lerp(transform.position, originalPosition, Time.deltaTime * returnSpeed);
             }
-        }
-
-
-        else if (isScrolling) { //Ensures that the user has finished pressing the up or scroll input.
-            returnTimer += Time.deltaTime; // update timer
-            if (returnTimer >= returnDelay) {
-                isScrolling = false; // stop scrolling
-            }
-        }
-
-        IEnumerator ReturnToStartPosition() {
-            yield return new WaitForSeconds(returnDelay);
-
-            while (Mathf.Round(transform.position.y) != Mathf.Round(startPositionY)) {
-                transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, startPositionY, transform.position.z), Time.deltaTime * returnSpeed);
-                yield return null;
-            }
-
-            returnRoutine = null;
         }
     }
 }
-
-
-
-
-
