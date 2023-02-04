16a5e74a9886900d69e80b86b6b519f29496953f
Jonathan Brown <jonathan.pavat.brown@gmail.com>
2023-02-05 10:31:52 +1300

    Implement water SFX

--- Assets/Scripts/Audio/MusicLoop.cs
+++ Assets/Scripts/Audio/MusicLoop.cs
@@ -2,7 +2,10 @@ using UnityEngine;
 
 public class MusicLoop : MonoBehaviour
 {
+	[SerializeField]
 	public AudioSource AudioSource;
+
+	[SerializeField]
 	public AudioClip BGMIntro;
 
 	// Start is called before the first frame update
---
+++ Assets/Scripts/Audio/SFXSingleton.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class SFXSingleton : MonoBehaviour
+{
+	public static SFXSingleton Instance => s_instance;
+	private static SFXSingleton s_instance = null;
+
+	[SerializeField]
+	public AudioSource AudioSource;
+
+	[SerializeField] public AudioClip WaterClip1;
+	[SerializeField] public AudioClip WaterClip2;
+	[SerializeField] public AudioClip WaterClip3;
+
+	private void Awake()
+	{
+		if (s_instance != null)
+		{
+			Destroy(gameObject);
+			return;
+		}
+
+		s_instance = this;
+	}
+
+	public void PlayWaterSFX()
+	{
+		int random = Random.Range(0, 3);
+		switch (random)
+		{
+			case 0: AudioSource.PlayOneShot(WaterClip1); break;
+			case 1: AudioSource.PlayOneShot(WaterClip2); break;
+			case 2: AudioSource.PlayOneShot(WaterClip3); break;
+		}
+	}
+}
