ecc498985bb0cddec7901d285330e19417018d88
Christopher Brown <christopher.chirat.brown@gmail.com>
2023-02-04 14:02:31 +1300

    Removing extra test code whoops

--- Assets/Scripts/RockTile.cs
+++ Assets/Scripts/RockTile.cs
@@ -4,11 +4,4 @@ using UnityEngine.Tilemaps;
 [CreateAssetMenu(fileName = "RockTile", menuName = "Tiles/RockTile")]
 public class RockTile : Tile
 {
-	[SerializeField]
-	private Sprite m_sprite;
-
-	public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData)
-	{
-		base.GetTileData(position, tilemap, ref tileData);
-	}
 }
