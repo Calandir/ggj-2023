302ecc97cf03f5bfd945b5f5333f68dcb2943c30
Christopher Brown <christopher.chirat.brown@gmail.com>
2023-02-04 15:44:25 +1300

    Water bar + water management

--- Assets/Scripts/WaterTile.cs
+++ Assets/Scripts/WaterTile.cs
@@ -4,4 +4,27 @@ using UnityEngine.Tilemaps;
 [CreateAssetMenu(fileName = "RockTile", menuName = "Tiles/WaterTile")]
 public class WaterTile : Tile
 {
+	[SerializeField]
+	private float m_startingWaterPerTile = 1.0f;
+
+	private float m_currentWater;
+
+	public override bool StartUp(Vector3Int position, ITilemap tilemap, GameObject go)
+	{
+		m_currentWater = m_startingWaterPerTile;
+
+		return base.StartUp(position, tilemap, go);
+	}
+
+	public void ConsumeWater()
+	{
+		if (m_currentWater <= 0.0f)
+		{
+			return;
+		}
+
+		WaterManager.Instance.AwardWater(m_currentWater);
+
+		m_currentWater = 0.0f;
+	}
 }
