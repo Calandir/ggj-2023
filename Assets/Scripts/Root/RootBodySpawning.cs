4e5367ab98ad8d35a8abef31d2c338ea987d6902
GAMING_LAPTOP\kalel <kalelovil@gmail.com>
2023-02-04 16:27:48 +1300

    - added splines package

---
+++ Assets/Scripts/Root/RootBodySpawning.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Splines;
+
+public class RootBodySpawning : MonoBehaviour
+{
+    [SerializeField]
+    private SplineContainer m_spline;
+}
