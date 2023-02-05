d287e531ecae584370442a8066c737ef93a1f497
Jonathan Brown <jonathan.pavat.brown@gmail.com>
2023-02-05 17:11:21 +1300

    Reset timer upon loading game scene

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -7,6 +7,13 @@ public class Timer : MonoBehaviour
 	[SerializeField]
 	private Text m_text;
 
+	private float m_timeStarted;
+
+	private void Start()
+	{
+		m_timeStarted = Time.time;
+	}
+
 	private void Update()
 	{
 		if (MiscUtils.IsGameOver)
@@ -14,7 +21,7 @@ public class Timer : MonoBehaviour
 			return;
 		}
 
-		float time = Time.time;
+		float time = Time.time - m_timeStarted;
 		float timeOneSignificantDigit = (float)(Math.Truncate(time * 10) / 10.0f);
 
 		string timeFormatted = string.Format("{0:0.0}", timeOneSignificantDigit);
