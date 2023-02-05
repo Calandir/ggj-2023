6bf4968d14c336132d12612c43aec8cc3a836d22
kalelovil <kalelovil@gmail.com>
2023-02-06 12:14:41 +1300

    - stream assets part  1

---
+++ Assets/Scripts/StreamTile.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(fileName = "StreamTile", menuName = "Tiles/StreamTile")]
+public class StreamTile : Tile
+{
+
+}
